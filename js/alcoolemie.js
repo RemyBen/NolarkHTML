faf16b4561717399756a17b56927e8038ead34a2
RemyBen <pntdu83@gmail.com>
2020-09-03 13:23:44 +0200

    add Function getString(id)

--- js/alcoolemie.js
+++ js/alcoolemie.js
@@ -86,3 +86,13 @@ function getInt(id){
         return valeur;
     }
 }
+
+/**
+ * Fonction qui retourne un string récupéré dans un champ via son id
+ * 
+ * @param {type} id
+ * @returns {Element.value}
+ */
+function getString(id){
+    return window.document.querySelector(id).value;
+}
