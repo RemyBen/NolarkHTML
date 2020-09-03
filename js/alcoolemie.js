bd8c11641fd6c40bbaec8175dbda0e23366974cf
RemyBen <pntdu83@gmail.com>
2020-09-03 11:30:09 +0200

    Update testalcoolemie finished

--- js/alcoolemie.js
+++ js/alcoolemie.js
@@ -8,13 +8,37 @@
 
 function getAlcoolPur(nbVerres){
     const uniteAlcool = 10;
-    return nbVerres * 10;
+    return nbVerres * uniteAlcool;
 }
 
+/**
+ * Fonction qui retourne le coefficient de diffusion en fonction du sexe
+ * 
+ * @param {type} sexe
+ * @returns {float}
+ */
 function getCoefDiffusion(sexe) {
+    const coefDiffuH = 0.7, coefDiffuF = 0.6;
     if (sexe === 'homme'){
-        return 0.7;
+        return coefDiffuH;
     } else {
-        return 0.6;
+        return coefDiffuF;
+    }
+}
+/**
+ * Fonction qui retourne l'alcoolémie en fonction du sexe, du poids et du
+ * nombre de verres ingérés
+ * 
+ * @param {type} sexe
+ * @param {type} poids
+ * @param {type} nbVerres
+ * @returns {float}
+ */
+function getAlcoolemie(sexe, poids, nbVerres){
+    // /!\ division par 0, on ne veut pas provoquer la destruction de l'univers ;0)
+    if (poids > 0){
+        return (getAlcoolPur(nbVerres) / (poids * getCoefDiffusion(sexe))).toFixed(2);
+    } else {
+        return 0;
     }
 }
--- test/unit/testAlcoolemie.js
+++ test/unit/testAlcoolemie.js
@@ -4,6 +4,7 @@
  */
 
 MesTestsUnitaires = TestCase('AlcoolemieTest');
+
 MesTestsUnitaires.prototype.testsGetAlcoolPur = function(){
     assertEquals('0 verre', 0, getAlcoolPur(0));
     assertEquals('1 verre', 10, getAlcoolPur(1));
@@ -12,4 +13,9 @@ MesTestsUnitaires.prototype.testsGetAlcoolPur = function(){
 MesTestsUnitaires.prototype.testsGetCoefDiffusion = function() {
     assertEquals('Homme', 0.7, getCoefDiffusion('homme'));
     assertEquals('Femme', 0.6, getCoefDiffusion('femme'));
+};
+
+MesTestsUnitaires.prototype.testsGetAlcoolemie = function() {
+    assertEquals('Homme 100 kg 1 verre', 0.14, getAlcoolemie('homme', 100, 1));
+    assertEquals('Femme 100 kg 1 verre', 0.17, getAlcoolemie('femme', 100, 1));
 };
