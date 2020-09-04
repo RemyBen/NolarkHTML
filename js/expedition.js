ce55a77adb87158ba721d2d1450f56314967c031
RemyBen <pntdu83@gmail.com>
2020-09-04 11:15:12 +0200

    add Test for Expedition + Method v2

--- js/expedition.js
+++ js/expedition.js
@@ -2,9 +2,15 @@ function getFrais(poids){
     if (poids <= 100){
         return (poids * 4.5).toFixed(2) + ' €' ;
     } else if (poids <= 1000) {
-        return (poids * 3).toFixed(2) + ' €';
+        somme = 100 * 4.5;
+        somme += (poids-100) * 3;
+        return (somme).toFixed(2) + ' €';
+        ;
     } else {
-        return (poids * 2.5).toFixed(2) + ' €';
+        somme = 100 * 4.5;
+        somme += 900 * 3;
+        somme += (poids - 1000) * 2.5;
+        return (somme).toFixed(2) + ' €';
     }
 
 }
